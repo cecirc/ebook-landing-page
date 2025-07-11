883baf611568eabecb5795ba336ec2a88750071b
Daniel <daniel.chagas@sicoob.com.br>
2025-07-11 19:36:53 -0300

    fix: corrige todos os caminhos absolutos para caminhos relativos

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 15452, hash: '0667ee8bcbc78d0a0089ddda5798fb45a54e0fedb241d546a1835511fc1479af', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14231, hash: '1144a0618d0e433de374a811af8615b973ed8de4e7b7653996b0fee86763bcc4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 51736, hash: 'eb9cfa3021db98688973c3486d758973a82e985c870a217e874ede19df9a6311', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-HYLYC7BG.css': {size: 23970, hash: '/mxzt7z15Lg', text: () => import('./assets-chunks/styles-HYLYC7BG_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 15452, hash: '9bf0e6555095a3f48bd07f35098ef14bcded67256c7916c4edfd56ba3de9c5f1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14231, hash: '996f1ee07371072a0e4876e92d6425f6c447339bb6bbceabdc070f5b70d42c92', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 51343, hash: '34f2b192b7c180950fc9d1a4109fe2852fed5c0b0a50d5429c9fcf13f1c954be', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-2ITY3OI6.css': {size: 23628, hash: 'aTgJo0QJGXs', text: () => import('./assets-chunks/styles-2ITY3OI6_css.mjs').then(m => m.default)}
   },
 };
