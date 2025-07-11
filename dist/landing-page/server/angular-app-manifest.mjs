1aed8a8ad98b7f07e12c92a3be4211cc14831b70
Daniel <daniel.chagas@sicoob.com.br>
2025-07-11 19:29:50 -0300

    fix: restore baseHref for GitHub Pages

--- dist/landing-page/server/angular-app-manifest.mjs
+++ dist/landing-page/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/ebook-landing-page/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/ebook-landing-page"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 15433, hash: '7a5b07dfab0d73b834c30e111bc130715fb5427cc6b5ab4d8ecd105666a77dbb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 14212, hash: 'c42c1ca65527c7b3e121145a0213ef093df602cb6ada7825d7a74d41bb498cc7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 51717, hash: '24758abbba9c40d174f5b86c61b7586179ec4b301cb8ea669391924f8dde2cb4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 15452, hash: '0667ee8bcbc78d0a0089ddda5798fb45a54e0fedb241d546a1835511fc1479af', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 14231, hash: '1144a0618d0e433de374a811af8615b973ed8de4e7b7653996b0fee86763bcc4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 51736, hash: 'eb9cfa3021db98688973c3486d758973a82e985c870a217e874ede19df9a6311', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HYLYC7BG.css': {size: 23970, hash: '/mxzt7z15Lg', text: () => import('./assets-chunks/styles-HYLYC7BG_css.mjs').then(m => m.default)}
   },
 };
--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/ebook-landing-page',
   supportedLocales: {
   "en-US": ""
 },
